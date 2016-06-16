3ea5ca8d55c877ed9be1a259ae16c7988daa8ef7
igrs <specialweekisno1@gmail.com>
2016-06-16 14:25:49 +0900

    update mix

--- mix.exs
+++ mix.exs
@@ -2,12 +2,20 @@ defmodule Readly.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :readly,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :readly,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      description: "Readly is a simple module that allow you to create readonly datasource module easily.",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: [
+        maintainers: ["igrs"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/igrs/readly"}
+      ],
+    ]
   end
 
   # Configuration for the OTP application
