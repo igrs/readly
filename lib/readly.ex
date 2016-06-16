6197867bc23f92a560da805905966182edb84c19
igrs <specialweekisno1@gmail.com>
2016-06-16 14:00:28 +0900

    add [get, all, reverse] function definition to 'readonly' macro

--- lib/readly.ex
+++ lib/readly.ex
@@ -1,30 +1,70 @@
 defmodule Readly do
+  @moduledoc ~S"""
+  This is Readly module.
+
+  ## Example
+      defmodule Gender do
+        use Readly, struct: %{id: nil, name: ""}
+
+        readonly %{id: 1, name: "Man"}, "man"
+        readonly %{id: 1, name: "Woman"}, :woman
+        readonly %{id: 1, name: "Trans"}, :trans
+      end
+  """
+
+  @doc false
   defmacro __using__([struct: struct]) do
     quote do
-      import Readly, only: [read_only: 2]
+      import Readly, only: [readonly: 2]
 
       defstruct Enum.map(unquote(struct), &(&1))
+
+      Module.register_attribute(__MODULE__, :readley_datasource, accumulate: true)
+
+      @before_compile Readly
     end
   end
 
-  defmacro read_only(item, field) do
+  @doc """
+  define readonly
+  """
+  defmacro readonly(item, function_name) do
     func_name = cond do
-      is_bitstring(field) -> String.to_atom(field)
-      is_atom(field) -> field
+      is_bitstring(function_name) -> String.to_atom(function_name)
+      is_atom(function_name) -> function_name
       true -> nil
     end
+
     if func_name do
       quote do
-        def unquote(field)() do
+        Module.put_attribute(__MODULE__, :readley_datasource, unquote(item))
+        def unquote(func_name)() do
           struct(__MODULE__, unquote(item))
         end
       end
     end
   end
-end
 
-#defmodule ReTest do
-#  use Readly, struct: %{en: "",  ja: "",   id: nil}
-#
-#  read_only %{en: "english",  ja: "英語",   id: 1}, :english
-#end
+  @doc false
+  defmacro __before_compile__(env) do
+    quote do
+      def all do
+        list
+        |> Enum.sort(fn a, b -> a.id < b.id end)
+      end
+
+      def reverse, do: all |> Enum.reverse
+
+      def get(nil), do: nil
+      def get(id) do
+        list
+        |> Enum.find(&(&1.id == id))
+      end
+
+      defp list do
+        @readley_datasource
+        |> Enum.map(fn source -> struct(unquote(env.module), source) end)
+      end
+    end
+  end
+end
--- test/readly_test.exs
+++ test/readly_test.exs
@@ -2,20 +2,52 @@ defmodule ReadlyTest do
   use ExUnit.Case
   doctest Readly
 
-  defmodule Mock do
+  defmodule Language do
     use Readly, struct: %{en: "", ja: "", id: nil}
 
-    read_only %{en: "english",  ja: "英語",   id: 1}, :english
-    read_only %{en: "chinese",  ja: "中国語", id: 2}, :chinese
-    read_only %{en: "japanese", ja: "日本語", id: 3}, :japanese
+    readonly %{en: "english",  ja: "英語",   id: 1}, :english
+    readonly %{en: "chinese",  ja: "中国語", id: 2}, "chinese"
+    readonly %{en: "japanese", ja: "日本語", id: 3}, :japanese
   end
 
-  test "english" do
-    assert Mock.english == %Mock{en: "english",  ja: "英語",   id: 1}
+  test "ensure build struct" do
+    assert %Language{} == %Language{en: "", ja: "", id: nil}
   end
 
+  test "automatic created function: english" do
+    assert Language.english == %Language{en: "english",  ja: "英語",   id: 1}
+  end
 
-  test "struct" do
-    assert %Mock{} == %Mock{en: "", ja: "", id: nil}
+  test "automatic created function: chinese" do
+    assert Language.chinese == %Language{en: "chinese",  ja: "中国語", id: 2}
+  end
+
+  test "automatic created function: japanese" do
+    assert Language.japanese == %Language{en: "japanese", ja: "日本語", id: 3}
+  end
+
+  test "ensure all datasource function" do
+    assert Language.all == [
+      %Language{en: "english",  ja: "英語",   id: 1},
+      %Language{en: "chinese",  ja: "中国語", id: 2},
+      %Language{en: "japanese", ja: "日本語", id: 3}
+    ]
+  end
+
+  test "ensure reverse datasource function" do
+    assert Language.reverse == [
+      %Language{en: "japanese", ja: "日本語", id: 3},
+      %Language{en: "chinese",  ja: "中国語", id: 2},
+      %Language{en: "english",  ja: "英語",   id: 1}
+    ]
+  end
+
+  test "ensure get function" do
+    assert Language.japanese == Language.get(3)
+    assert Language.chinese == Language.get(2)
+  end
+
+  test "ensure get function when unknown id" do
+    assert nil == Language.get(5)
   end
 end
