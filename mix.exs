6478a7c899bcd83afe41543e540c306b18687cad
igrs <specialweekisno1@gmail.com>
2016-06-17 16:38:37 +0900

    add ecto to deps only test

--- mix.exs
+++ mix.exs
@@ -35,6 +35,9 @@ defmodule Readly.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.12", only: :dev}]
+    [
+      {:ex_doc, "~> 0.12", only: :dev},
+      {:ecto,   "~> 1.1",  only: :test}
+    ]
   end
 end
